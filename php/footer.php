90e495af52ac9bba1bf9b98c404312349310158b
BeerOfSteel <florian.reumont@me.com>
2022-09-20 15:34:53 +0200

    t

--- php/footer.php
+++ php/footer.php
@@ -1,37 +1,39 @@
-        <div class="footer-top">
-            <div class="footer-top-card">
-                <h4 class="footer-top-card-title">Nous Contacter</h4>
-                <p class="footer-top-card-content">1 Rue du Dahomey<br>75011 Paris<br>xxxxxx.xxxxxx@ecv.fr<br>01 85 08
-                    26 86</p>
-                <img src="img/logo.png" alt="Frost Delivery Tracker Logo" class="logo" draggable="false">
-            </div>
-            <div class="footer-top-card">
-                <h4 class="footer-top-card-title">Frost Delivery Tracker</h4>
-                <div class="footer-top-card-content">
-                    <a href="engagements.php">Nos engagements</a>
-                    <a href="partenaires.php">Nos partenaires</a>
-                    <a href="apropos.php">À propos de nous</a>
-                    <a href="conditions.php">Conditions générales</a>
-                    <a href="faq.php">FAQ / Aide</a>
+<div class="footer">
+            <div class="footer-top">
+                <div class="footer-top-card">
+                    <h4 class="footer-top-card-title">Nous Contacter</h4>
+                    <p class="footer-top-card-content">1 Rue du Dahomey<br>75011 Paris<br>xxxxxx.xxxxxx@ecv.fr<br>01 85 08
+                        26 86</p>
+                    <img src="img/logo.png" alt="Frost Delivery Tracker Logo" class="logo" draggable="false">
+                </div>
+                <div class="footer-top-card">
+                    <h4 class="footer-top-card-title">Frost Delivery Tracker</h4>
+                    <div class="footer-top-card-content">
+                        <a href="engagements.php">Nos engagements</a>
+                        <a href="partenaires.php">Nos partenaires</a>
+                        <a href="apropos.php">À propos de nous</a>
+                        <a href="conditions.php">Conditions générales</a>
+                        <a href="faq.php">FAQ / Aide</a>
+                    </div>
+                </div>
+                <div class="footer-top-card">
+                    <h4 class="footer-top-card-title">Adhérer à notre produit</h4>
+                    <div class="footer-top-card-content">
+                        <a href="engagements.php">Nos engagements</a>
+                        <a href="adherer.php">Adhérer</a>
+                        <a href="faq.php">FAQ</a>
+                    </div>
+                </div>
+                <div class="footer-top-card">
+                    <h4 class="footer-top-card-title">Menu</h4>
+                    <div class="footer-top-card-content">
+                        <a href="index.php">Accueil</a>
+                        <a href="apropos.php">À propos</a>
+                        <a href="partenaires.php">Partenaires</a>
+                        <a href="adherer.php">Adhérer</a>
+                        <a href="faq.php">FAQ</a>
+                    </div>
                 </div>
             </div>
-            <div class="footer-top-card">
-                <h4 class="footer-top-card-title">Adhérer à notre produit</h4>
-                <div class="footer-top-card-content">
-                    <a href="engagements.php">Nos engagements</a>
-                    <a href="adherer.php">Adhérer</a>
-                    <a href="faq.php">FAQ</a>
-                </div>
-            </div>
-            <div class="footer-top-card">
-                <h4 class="footer-top-card-title">Menu</h4>
-                <div class="footer-top-card-content">
-                    <a href="index.php">Accueil</a>
-                    <a href="apropos.php">À propos</a>
-                    <a href="partenaires.php">Partenaires</a>
-                    <a href="adherer.php">Adhérer</a>
-                    <a href="faq.php">FAQ</a>
-                </div>
-            </div>
-        </div>
-        <span class="footer-bottom">© 2022 Frost Delivery Tracker - Tous droits réservés</span>
+            <span class="footer-bottom">© 2022 Frost Delivery Tracker - Tous droits réservés</span>
+</div>
--- php/header.php
+++ php/header.php
@@ -1,12 +1,14 @@
-        <a href="index.php"><img src="img/logo.png" alt="Frost Delivery Tracker Logo" class="logo"
-                draggable="false"></a>
+        <div class="header">
+                <a href="index.php"><img src="img/logo.png" alt="Frost Delivery Tracker Logo" class="logo"
+                        draggable="false"></a>
 
-        <nav>
-            <a href="index.php">Accueil</a>
-            <a href="apropos.php">À propos</a>
-            <a href="partenaires.php">Partenaires</a>
-            <a href="adherer.php">Adhérer</a>
-            <a href="faq.php">FAQ</a>
-        </nav>
+                <nav>
+                <a href="index.php">Accueil</a>
+                <a href="apropos.php">À propos</a>
+                <a href="partenaires.php">Partenaires</a>
+                <a href="adherer.php">Adhérer</a>
+                <a href="faq.php">FAQ</a>
+                </nav>
 
-        <a href="login.php"><img src="img/user.png" alt="login" class="user-login"></a>
+                <a href="login.php"><img src="img/user.png" alt="login" class="user-login"></a>
+        </div>
