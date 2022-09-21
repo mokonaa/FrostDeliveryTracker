a036f68c1af2a206c67ffc808cd6e983fb802e21
BeerOfSteel <florian.reumont@me.com>
2022-09-21 14:31:55 +0200

    page engagement

---
+++ pageEngagements/engagements.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include("../php/head.php")?>
+    <title>Nos engagements</title>
+    <link rel="stylesheet" href="./engagements.css">
+</head>
+
+<body>
+    <?php include("../php/header.php")?>
+
+
+
+    <section class="nos-engagements">
+
+        <div class="nos-engagements-globaux">
+            <h1>Les avantages</h1>
+            <div class="nos-engagements-globaux-container">
+                <div class="nos-engagements-globaux-container-card">
+                    <img src="./img/haute-qualite.png" alt="">
+                    <h2>Un transport sécurisé </h2>
+                    <p>Nous faisons appel aux meilleurs transporteurs pour assurer la sécurité de vos colis</p>
+                </div>
+                <div class="nos-engagements-globaux-container-card">
+                    <img src="./img/freezing.png" alt="">
+                    <h2>Des produits toujours frais à l’arrivée </h2>
+                    <p>Grâce à notre système nous pouvons contrôler la température des produits lors du transport</p>
+                </div>
+                <div class="nos-engagements-globaux-container-card">
+                    <img src="./img/delivery.png" alt="">
+                    <h2>Un suivi de vos colis</h2>
+                    <p> Vous pourrez suivre le cheminement de vos colis en temps réel grâce à notre système de localisation</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="nos-engagements-card">
+            <h3>Chaque membre de notre équipe est hautement qualifié dans son domaine</h3>
+            <p>Des étudiants de l'ECV Digital à Paris, chargés de réaliser un projet en 1 semaine se sont regroupés
+                pour vous proposer le meilleur service possible. Allant de la réalisation du produit à la création du
+                site internet, en passant par l'implémentation de la base de données, nous avons une vraie équipe
+                touche à tout.
+            </p>
+        </div>
+        <div class="nos-engagements-card">
+            <h3>Le meilleur rapport qualité/prix sur le marché</h3>
+            <p>Nous sommes persuadés que ce n’est pas parce que vous achetez des pièces bon marché que vous ne devriez
+                pas pouvoir bénéficier d’une qualité irréprochable. C’est pour cette raison que nous faisons notre
+                maximum pour vous proposer des produits à des prix défiant toute concurrence, sans pour autant sacrifier
+                leur qualité, leur fiabilité, ou leur durabilité. Quelle que soit la pièce que vous acheterez sur notre
+                site, elle respectera toujours le cahier des charges constructeur !
+                <br><br>
+                Pour vous proposer le meilleur rapport qualité/prix sur le marché, nous faisons tout simplement un grand
+                travail de sélection de chacune de nos références, en nous assurant qu’elles respectent un niveau de
+                sécurité optimal, soient faciles à entretenir, et disposent de plusieurs options de réparation. Le tout
+                avec les tarifs les plus bas, et de nombreuses promotions pour nos clients les plus fidèles !
+            </p>
+        </div>
+    </section>
+
+
+    <?php include("../php/footer.php")?>
+</body>
+
+</html>
