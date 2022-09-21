1baf0b4252d6612a756a8b58a7224bfd0d576762
JKxKookie <mariaguydefontgalland@gmail.com>
2022-09-21 14:48:16 +0200

    points affiché sur la carte leaflet

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log('help')
+
+// heure ( --> voir le temps comment c'est géré - fluide)
+const temps = [
+    '20',
+    '-15',
+    '-10',
+    '-5',
+    '0',
+    '5',
+    '10',
+    '15',
+    '20',
+    
+];
+
+
+var celsius = [0, 10, 5, 2, 20, 30, 45]; // température fixe}]
+var fahrenheit = [0, 10, 5, 2, 20, 30, 45]; // température fixe}]
+
+const data = {
+    labels: temps,
+    datasets: [{
+      label: 'Température colis en celsius',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: celsius, // température fixe
+    }],
+};
+
+// prévoir un bouton farenheight ->  celsius
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
