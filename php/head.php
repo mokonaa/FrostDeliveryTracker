04a02256de5ad30942c1aeaa400400f7d93b6a2b
Florian <79874636+BeerOfSteel@users.noreply.github.com>
2022-09-21 16:28:16 +0200

    Update head.php

--- php/head.php
+++ php/head.php
@@ -1,7 +1,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" sizes="64x64" href="./img/favicon.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="../img/favicon.png">
     <meta name="description"
         content="Frost Delivery Tracker, une solution simple pour que vos livraisons arrivent sans aucun problème.">
 
