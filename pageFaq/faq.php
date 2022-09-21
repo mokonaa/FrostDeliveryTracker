908b89b7bce3aff8d8b805ed018ecd9dac36c822
Laure_Sechet <laure.sechet@hotmail.com>
2022-09-21 16:14:34 +0200

    ajout page FAQ

---
+++ pageFaq/faq.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <?php include("../php/head.php")?>
+    <title>FAQ Frost Delivery Tracker</title>
+    <link rel="stylesheet" href="faq.css">
+
+</head>
+<body>
+    <?php include("../php/header.php")?>
+
+
+
+
+   <article>
+        <details close>
+            <summary>
+                <h2>Comment commander le boîtier ?</h2>
+                <span>+</span>
+            </summary>
+            <p>Sur la page adhérer, vous devez remplir le formulaire de contact en indiquant votre demande ainsi que le nombre de boîtiers que vous désirez. </p>
+        </details>
+        <details close>
+            <summary> 
+                <h2>Comment avoir un login et un mot de passe ?</h2>
+                <span>+</span>
+            </summary>
+            <p>À la réception de votre boîtier, vous recevrez en même temps votre login et mot de passe.</p>
+        </details>
+        <details close>
+            <summary>
+                <h2>Comment suivre la livraison de mon colis ?</h2>
+                <span>+</span>
+            </summary>
+            <p> En vous connectant à votre espace, vous pourrez accéder à la page pour suivre votre colis. Ensuite, il ne restera plus qu’à entrer votre numéro de colis.</p>
+        </details>
+        <details close>    
+            <summary>
+                <h2>Comment être sur que la température de mon colis a été respectée ?</h2>
+                <span>+</span>
+            </summary>
+            <p>Si au cours de la livraison nous rencontrons un problème de température, vous recevrez un message d’alerte vous indiquant combien de temps votre colis est resté en dehors des températures renseignée.</p>
+        </details>
+ 
+</article>
+
+
+    
+    <?php include("../php/footer.php")?>
+
+</body>
+</html>
