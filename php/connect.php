15300ee2ee1b5806398d0814697dfe82f8420b63
BeerOfSteel <florian.reumont@me.com>
2022-09-22 19:14:49 +0200

    ne pas prendre en compte

--- php/connect.php
+++ php/connect.php
@@ -1,17 +1,17 @@
 <?php   
 
     $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $base = "testbdd";
+    $user = "sql_ecv_aammara";
+    $pass = "WwCpJC9N2Q";
+    $base = "base_ecv_aammara";
 
     $conn = mysqli_connect($host,$user,$pass,$base);
 
     // Détection d'erreur bdd
-    // if(mysqli_connect_error())
-    // {
-    //     echo "Non connecté à la BDD.";
-    // }
+    if(mysqli_connect_error())
+    {
+        echo "<script>alert('Non connecté à la BDD.');</script>";
+    }
     // else
     // {
     //     echo "Connecté à la BDD";
