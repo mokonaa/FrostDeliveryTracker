ff9d87b847ad8af4c30b7410a625062fd1cdc58c
JKxKookie <mariaguydefontgalland@gmail.com>
2022-09-22 16:26:59 +0200

    suivi temp real data

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log("coucoucocuo");
 const temps = [
     '20',
     '-15',
@@ -15,8 +14,9 @@ const temps = [
 // recup donnes températures depuis le json + requete
 console.log(json, 'the json obj');
 
+
 var celsius = [0, 10, 5, 2, 20, 30, 45]; // température fixe}]
-var fahrenheit = [0, 10, 5, 2, 20, 30, 45]; // température fixe}]
+var fahrenheit = [10, 10, 50, 2, 20, 30, 45]; // température fixe}]
 
 const data = {
     labels: temps,
@@ -28,6 +28,26 @@ const data = {
     }],
 };
 
+const data2 = {
+    labels: temps,
+    datasets: [
+      {
+        label: 'Celcius',
+        data: celsius,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgb(255, 99, 132)',
+        yAxisID: 'y',
+      },
+      {
+        label: 'Fahrenheit',
+        data: fahrenheit,
+        borderColor: 'rgb(005, 99, 132)',
+        backgroundColor: 'rgb(005, 99, 132)',
+        yAxisID: 'y1',
+      }
+    ]
+  };
+
 // prévoir un bouton farenheight ->  celsius
 
 const config = {
@@ -36,9 +56,46 @@ const config = {
     options: {}
 };
 
+
+const config2 = {
+    type: 'line',
+    data: data2,
+    options: {
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: 'Suivi température'
+        }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+  
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      }
+    },
+  };
+
 const myChart = new Chart(
     document.getElementById('myChart'),
-    config
+    config2
 );
 
 
